0890ace733480c6ee9606df23966ca256f46a7da
Clifford Mapesa <cliffordmapesa8@gmail.com>
2022-10-28 12:02:44 +0300

    Refactored code

--- index.js
+++ index.js
@@ -1,49 +1,43 @@
-let homeScores = document.getElementById('homescores');
-let guestScores = document.getElementById('guestscores');
+let homeScore = 0;
+let guestScore = 0;
 
-let count = 0;
-let guestCount = 0;
 
-function restart() {
-  
+const container = document.querySelector('.container');
+const homeDisplay = document.getElementById('home-display');
+const guestDisplay = document.getElementById('guest-display');
+const newGame = document.querySelector('#new-game')
+
+container.addEventListener('click', function (e) {
+  const target = e.target
+  const homePts = target.dataset.homePts
+  const guestPts = target.dataset.guestPts
+
+  if (homePts) {
+    homeScore += +homePts
+    homeDisplay.textContent = homeScore
+  }
+
+  if (guestPts) {
+    guestScore += +guestPts
+    guestDisplay.textContent = guestScore
+  }
+
+  highlight()
+})
+
+function highlight() {
+  homeDisplay.classList.toggle('highlighter', homeScore > guestScore)
+  guestDisplay.classList.toggle('highlighter', homeScore < guestScore)
 }
 
-function add1Point() {
-  count += 1;
-  homeScores.textContent = count;  
-}
-
-function add2Points() {
-  count += 2;
-  homeScores.textContent = count;
-}
-
-function add3Points() {
-  count += 3;
-  homeScores.textContent = count;
-}
-
-function add1PointGuest() {
-  guestCount += 1;
-  guestScores.textContent = guestCount;
-}
-
-function add2PointsGuest() {
-  guestCount += 2;
-  guestScores.textContent = guestCount;
-}
-
-function add3PointsGuest() {
-  guestCount += 3;
-  guestScores.textContent = guestCount;
-}
-
-function newGame() {
-  count = 0;
-  guestCount = 0;
-  homeScores.textContent = count;
-  guestScores.textContent = guestCount
-}
+newGame.addEventListener('click', function () {
+  homeScore = 0
+  guestScore = 0
+  homeDisplay.textContent = 0
+  guestDisplay.textContent = 0
+  homeDisplay.classList.remove('highlighter')
+  guestDisplay.classList.remove('highlighter')
+})
 
 
 
